b0c7f3779c3b51a11402aa066fdb390270723bed
Quinn Anderson <162663746+quinnanderson1@users.noreply.github.com>
2024-03-31 20:13:54 -0400

    Update Day2.py

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,1 +1,9 @@
+# The purpose of today's project is to create a tip calculator that
+# takes into account the total, tip, and number of people paying
 
+print("Welcome to the tip calculator!")
+total = float(input("What was the total bill? $"))
+tip = float(input("How much tip would you like to give? 10, 12, or 15? ")) / 100
+numPeople = int(input("How many ways would you like to split the bill? "))
+totalToPay = (total + (total * tip)) / numPeople
+print(f"Each person should pay: ${totalToPay:.2f}")
